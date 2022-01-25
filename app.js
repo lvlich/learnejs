2fa7ea259aafb322dba6c2b584c052059b69e7c7
lvlich <lvlich@fossil.com>
2022-01-25 19:52:52 +0700

    Add Todo work feature

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const app = express();
+const path = require("path");
 let ejs = require("ejs");
 
-app.get("/", function(req, res) {
-    const day = new Date();
-    const dayOfWeek = day.getDay();
-    console.log(dayOfWeek);
-    res.sendFile(__dirname + "/index.html");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
+
+const ITEMS = [];
+
+app.get("/", function (req, res) {
+    var today = date.getDate();
+    var x = { today: today, newItem: ITEMS }
+    
+    console.log(JSON.stringify(x));
+    res.render("list", x);
 });
 
+app.post("/", function (req, res) {
+    ITEMS.push(req.body.item)
+    res.redirect("/");
+})
 
-app.listen(3000, function() {
+app.get("/about", function (req, res) {
+    res.render("about");
+})
+
+app.listen(3000, function () {
     console.log("Start listening on port 3000");
 });
